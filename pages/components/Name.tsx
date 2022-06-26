d8843fa8db6b4914ea196058d95eb605fa5adcea
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-06-26 18:52:03 +0500

    name component

--- pages/components/Name.tsx
+++ pages/components/Name.tsx
@@ -2,27 +2,27 @@ import React from "react";
 import styles from "../../styles/Form.module.css";
 
 interface props {
-  formData: any;
-  setFormData: React.Dispatch<React.SetStateAction<string>>;
-  lastName: any;
-  firstName: any;
+  formValue?: any;
+  setformValue?: any;
 }
-const Name = ({ formData, setFormData }: props) => {
+const Name = ({ formValue, setformValue }: props) => {
   return (
     <>
       <p className={styles.subheading}>Name</p>
       <br />
       <input
-        value={formData.firstName}
+        value={formValue.firstName}
         onChange={(e) =>
-          setFormData({ ...formData, firstName: e.target.value })
+          setformValue({ ...formValue, firstName: e.target.value })
         }
         placeholder="First Name"
         className={styles.input}
       />
       <input
-        value={formData.lastName}
-        onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+        value={formValue.lastName}
+        onChange={(e) =>
+          setformValue({ ...formValue, lastName: e.target.value })
+        }
         placeholder="Last Name"
         className={styles.input}
       />
