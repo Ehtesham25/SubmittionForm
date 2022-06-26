405707d55e4c733b50c7db2e50e8267b92b59963
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-06-26 18:51:50 +0500

    index js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,52 @@
 import React, { useState } from "react";
-import { useRouter } from "next/router";
 
 import styles from "../styles/Home.module.css";
+import styles1 from "../styles/Home.module.css";
 import { Progress, Name } from "./components";
+import Email from "./Email";
+import Details from "./Details";
 
 const Home = () => {
-  const router = useRouter();
-  const [formData, setFormData]: any = useState({
+  const [formValue, setFormValue] = useState({
     firstName: "",
     lastName: "",
+    email: "",
   });
+  const [nameForm, setNameForm] = useState(true);
+  const [emailForm, setEmailForm] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState(false);
 
-  const handleSubmit = () => {
-    if (!formData.firstName || !formData.lastName) {
-      alert("please, fill all field");
-    } else {
-      router.push("/Email");
-      localStorage.setItem("firstName", formData.firstName);
-      localStorage.setItem("lastName", formData.lastName);
+  const handleNext = () => {
+    if (nameForm) {
+      if (!formValue.firstName && !formValue.lastName) {
+        alert("please Fill all field!!");
+      } else {
+        setNameForm(false);
+        setEmailForm(true);
+      }
+    } else if (emailForm) {
+      if (!formValue.email) {
+        alert("please Fill email field!!");
+      } else {
+        setNameForm(false);
+        setEmailForm(false);
+        setConfirmationMessage(true);
+      }
+    } else if (confirmationMessage) {
+      alert("Form Submitted Thank You..");
+      setNameForm(true);
+      setEmailForm(false);
+      setConfirmationMessage(false);
+      setFormValue(" ");
+    }
+  };
+
+  const handlePrevous = () => {
+    setNameForm(true), setEmailForm(false);
+    if (confirmationMessage) {
+      setNameForm(false);
+      setConfirmationMessage(false);
+      setEmailForm(true);
     }
   };
 
@@ -26,26 +55,48 @@ const Home = () => {
       <div className={styles.main}>
         <h3 className={styles.heading}>WIZARD</h3>
         <div className={styles.card}>
-          <Progress number={1} formName="Name" color="#25B8D0" />
-          <Progress number={2} formName="Email" color="#646464" />
-          <Progress number={3} formName="Confirmation" color="#646464" />
-        </div>
-        <div className={styles.wrapper}>
-          <Name
-            formData={formData}
-            setFormData={setFormData}
-            firstName={formData.firstName}
-            lastName={formData.lastName}
+          <Progress number={1} name="Name" color="#25B8D0" />
+          <Progress
+            number={2}
+            name="Email"
+            color={emailForm ? "#25B8D0" : "#646464"}
+          />
+          <Progress
+            number={3}
+            name="Confirmation"
+            color={confirmationMessage ? "#25B8D0" : "#646464"}
           />
         </div>
-        <div className={styles.container1}>
+        <div className={styles.wrapper}>
+          {nameForm && (
+            <Name formValue={formValue} setformValue={setFormValue} />
+          )}
+
+          {emailForm && (
+            <Email formValue={formValue} setformValue={setFormValue} />
+          )}
+          {confirmationMessage && <Details formValue={formValue} />}
+        </div>
+
+        <div className={styles1.container1}>
+          <button
+            onClick={() => handlePrevous()}
+            className={styles1.btn}
+            style={{
+              backgroundColor: "#c0c5cb",
+              cursor: nameForm ? "not-allowed" : "pointer",
+            }}
+            disabled={nameForm}
+          >
+            Back
+          </button>
           <button
             onClick={() => {
-              handleSubmit();
+              handleNext();
             }}
-            className={styles.btn}
+            className={styles1.btn}
           >
-            NEXT
+            {confirmationMessage ? "SUBMIT" : "NEXT"}
           </button>
         </div>
       </div>
